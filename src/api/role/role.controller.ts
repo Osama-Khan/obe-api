118758b6810bc6499bc10c921aff989914bed859
osama-khan <osama6756@gmail.com>
2021-10-15 17:07:52 +0500

    Added Role functionality

--- src/api/role/role.controller.ts
+++ src/api/role/role.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { CrudController } from '@shared/controllers/crud.controller';
+import { RoleEntity } from './role.entity';
+import { RoleService } from './role.service';
 
 @Controller('role')
-export class RoleController {}
+export class RoleController extends CrudController<RoleEntity, RoleService> {
+  constructor(@Inject(RoleService) service: RoleService) {
+    super(service);
+  }
+}
--- src/api/role/role.service.ts
+++ src/api/role/role.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ApiService } from '@shared/services/api.service';
+import { Repository } from 'typeorm';
+import { RoleEntity } from './role.entity';
 
 @Injectable()
-export class RoleService {}
+export class RoleService extends ApiService<RoleEntity> {
+  constructor(
+    @InjectRepository(RoleEntity) repository: Repository<RoleEntity>,
+  ) {
+    super(repository);
+  }
+}
