ace2351517c24fce3d8e88cc680e4d44c6081877
osama-khan <osama6756@gmail.com>
2021-10-15 14:20:17 +0500

    Added token and user data return on login

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -4,6 +4,7 @@ import { ApiService } from '@shared/services/api.service';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
 import * as bcrypt from 'bcrypt';
+import { JwtHelper } from '@shared/helpers';
 
 @Injectable()
 export class UserService extends ApiService<UserEntity> {
@@ -14,10 +15,12 @@ export class UserService extends ApiService<UserEntity> {
   }
 
   async login(data: Pick<UserEntity, 'username' | 'password'>) {
-    const user = await this.repository.findOne({ username: data.username });
-    if (!user) throw new BadRequestException('No such user!');
-    if (await bcrypt.compare(data.password, user.password)) {
-      return {};
+    const u = await this.repository.findOne({ username: data.username });
+    if (!u) throw new BadRequestException('No such user!');
+    if (await bcrypt.compare(data.password, u.password)) {
+      const { password, ...user } = u;
+      const token = JwtHelper.sign({ id: u.id, isAdmin: u.isAdmin });
+      return { ...user, token };
     }
     throw new BadRequestException('Invalid password!');
   }
