0eb8922265afb5cc28f0d561705fb76fd3eccbf0
osama-khan <osama6756@gmail.com>
2021-10-15 17:07:20 +0500

    Fixed provider import

--- src/api/role/role.module.ts
+++ src/api/role/role.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleController } from './role.controller';
 import { RoleEntity } from './role.entity';
+import { RoleService } from './role.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RoleEntity])],
   controllers: [RoleController],
-  providers: [RoleEntity],
+  providers: [RoleService],
 })
 export class RoleModule {}
