c562e188db58812731a6fd8a1bbd0ef0557f12f0
osama-khan <osama6756@gmail.com>
2021-10-16 16:16:57 +0500

    Added insert method for plo insertion

--- src/api/program/program.module.ts
+++ src/api/program/program.module.ts
@@ -1,3 +1,5 @@
+import { PLOEntity } from '@api/objective/plo/plo.entity';
+import { PLOService } from '@api/objective/plo/plo.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProgramController } from './program.controller';
@@ -5,8 +7,8 @@ import { ProgramEntity } from './program.entity';
 import { ProgramService } from './program.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProgramEntity])],
+  imports: [TypeOrmModule.forFeature([ProgramEntity, PLOEntity])],
   controllers: [ProgramController],
-  providers: [ProgramService],
+  providers: [ProgramService, PLOService],
 })
 export class ProgramModule {}
--- src/api/program/program.service.ts
+++ src/api/program/program.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { PLOService } from '@api/objective/plo/plo.service';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApiService } from '@shared/services/api.service';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { ProgramEntity } from './program.entity';
 
 @Injectable()
 export class ProgramService extends ApiService<ProgramEntity> {
   constructor(
     @InjectRepository(ProgramEntity) repository: Repository<ProgramEntity>,
+    @Inject(PLOService) private ploService: PLOService,
   ) {
     super(repository);
+  }
+
+  async insert(data: DeepPartial<ProgramEntity>) {
+    const { plos, ...prog } = data;
+    const program = await super.insert(prog);
+    for (let i in plos) {
+      await this.ploService.insert({ ...data.plos[i], program });
+    }
+    return { plos, ...program };
   }
 }
