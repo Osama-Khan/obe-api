091e56e50a71b882c74aafa006439a2ed2c9ce5b
Osama Khan <Osama6756@gmail.com>
2021-11-12 15:29:04 +0500

    Removed custom insertion method

--- src/api/program/program.service.ts
+++ src/api/program/program.service.ts
@@ -2,7 +2,7 @@ import { PLOService } from '@api/objective/plo/plo.service';
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApiService } from '@shared/services/api.service';
-import { DeepPartial, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { ProgramEntity } from './program.entity';
 
 @Injectable()
@@ -12,14 +12,5 @@ export class ProgramService extends ApiService<ProgramEntity> {
     @Inject(PLOService) private ploService: PLOService,
   ) {
     super(repository);
-  }
-
-  async insert(data: DeepPartial<ProgramEntity>) {
-    const { plos, ...prog } = data;
-    const program = await super.insert(prog);
-    for (let i in plos) {
-      await this.ploService.insert({ ...data.plos[i], program });
-    }
-    return { plos, ...program };
   }
 }
