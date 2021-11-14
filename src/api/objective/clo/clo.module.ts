8e5868cf2de284a29b8ed919c71b396e6b7e5aa9
Osama Khan <Osama6756@gmail.com>
2021-11-14 19:09:52 +0500

    Improved find in CLOService to return detailed map

--- src/api/objective/clo/clo.module.ts
+++ src/api/objective/clo/clo.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ObjectiveMapEntity } from '../map/map.entity';
 import { CLOController } from './clo.controller';
 import { CLOEntity } from './clo.entity';
 import { CLOService } from './clo.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CLOEntity])],
+  imports: [TypeOrmModule.forFeature([CLOEntity, ObjectiveMapEntity])],
   controllers: [CLOController],
   providers: [CLOService],
 })
--- src/api/objective/clo/clo.service.ts
+++ src/api/objective/clo/clo.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApiService } from '@shared/services/api.service';
-import { Repository } from 'typeorm';
+import { FindManyOptions, In, Repository } from 'typeorm';
+import { ObjectiveMapEntity } from '../map/map.entity';
 import { CLOEntity } from './clo.entity';
 
 @Injectable()
 export class CLOService extends ApiService<CLOEntity> {
-  constructor(@InjectRepository(CLOEntity) repository: Repository<CLOEntity>) {
+  constructor(
+    @InjectRepository(CLOEntity) repository: Repository<CLOEntity>,
+    @InjectRepository(ObjectiveMapEntity)
+    private omRepository: Repository<ObjectiveMapEntity>,
+  ) {
     super(repository);
+  }
+
+  /** Appends complete map objects if maps relation is included */
+  async find(criteria?: FindManyOptions<CLOEntity>): Promise<CLOEntity[]> {
+    const data = await this.repository.find(criteria);
+    const clos = [];
+    if (criteria?.relations.includes('maps')) {
+      for (const c of data) {
+        const maps = await this.omRepository.find({
+          where: { id: In(c.maps.map((m) => m.id)) },
+          relations: ['plo'],
+        });
+        clos.push({ ...c, maps });
+      }
+    }
+    return clos.length > 0 ? clos : data;
   }
 }
