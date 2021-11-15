3c54dd4f3b39d1f6c93b7d9efae4efa7a7c54ed3
Osama Khan <Osama6756@gmail.com>
2021-11-15 16:11:11 +0500

    Removed login from UserModule

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,16 +1,11 @@
 import { CrudController } from '@shared/controllers/crud.controller';
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController extends CrudController<UserEntity, UserService> {
-  constructor(@Inject(UserService) private userService: UserService) {
+  constructor(@Inject(UserService) userService: UserService) {
     super(userService);
-  }
-
-  @Post('login')
-  login(@Body() data: Pick<UserEntity, 'password' | 'username'>) {
-    return this.userService.login(data);
   }
 }
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,10 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApiService } from '@shared/services/api.service';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
-import * as bcrypt from 'bcrypt';
-import { JwtHelper } from '@shared/helpers';
 
 @Injectable()
 export class UserService extends ApiService<UserEntity> {
@@ -12,19 +10,5 @@ export class UserService extends ApiService<UserEntity> {
     @InjectRepository(UserEntity) repository: Repository<UserEntity>,
   ) {
     super(repository);
-  }
-
-  async login(data: Pick<UserEntity, 'username' | 'password'>) {
-    const u = await this.repository.findOne(
-      { username: data.username },
-      { relations: ['role'] },
-    );
-    if (!u) throw new BadRequestException('No such user!');
-    if (await bcrypt.compare(data.password, u.password)) {
-      const { password, ...user } = u;
-      const token = JwtHelper.sign({ id: u.id, role: u.role });
-      return { ...user, token };
-    }
-    throw new BadRequestException('Invalid password!');
   }
 }
