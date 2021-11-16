9e09223935957863a445da24d05f01808cdbc7b0
osama-khan <osama6756@gmail.com>
2021-11-16 17:36:37 +0500

    improved user seeding, Added roles on users

--- src/database/user/user-seeder.module.ts
+++ src/database/user/user-seeder.module.ts
@@ -1,4 +1,5 @@
 import { RoleEntity } from '@api/role/role.entity';
+import { RoleService } from '@api/role/role.service';
 import { UserEntity } from '@api/user/user.entity';
 import { UserService } from '@api/user/user.service';
 import { Module } from '@nestjs/common';
@@ -6,6 +7,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, RoleEntity])],
-  providers: [UserService],
+  providers: [UserService, RoleService],
 })
 export class UserSeederModule {}
--- src/database/user/user-seeder.ts
+++ src/database/user/user-seeder.ts
@@ -1,9 +1,23 @@
+import { RoleService } from '@api/role/role.service';
 import { UserService } from '@api/user/user.service';
 import { INestApplication } from '@nestjs/common';
+import { LogHelper } from '@shared/helpers';
 import seed from '../helpers';
 import * as users from './users.json';
 
 export default async function seedUsers(app: INestApplication) {
   const svc = app.get(UserService);
-  await seed(svc, users, 'user');
+  const rsvc = app.get(RoleService);
+  const roleUsers = [];
+  for (const u of users) {
+    const roles = await rsvc.find({ where: { name: u.role } });
+    if (roles.length === 0) {
+      LogHelper.error(`No such role "${u.role}"`);
+      throw new Error(
+        `The role "${u.role}" was not found. Please make sure the role name is correct in users seed file and that roles are seeded before users.`,
+      );
+    }
+    roleUsers.push({ ...u, role: { id: roles[0].id } });
+  }
+  await seed(svc, roleUsers, 'user');
 }
