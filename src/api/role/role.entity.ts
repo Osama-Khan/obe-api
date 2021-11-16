493a5cc11392eabac814cfb548e5e006eb532f18
Osama Khan <Osama6756@gmail.com>
2021-11-16 19:23:04 +0500

    Changed role name type to string

--- src/api/role/role.entity.ts
+++ src/api/role/role.entity.ts
@@ -2,12 +2,10 @@ import { UserEntity } from '@api/user/user.entity';
 import { ParentEntity } from '@shared/entity/ParentEntity';
 import { Column, Entity, OneToMany } from 'typeorm';
 
-type RoleType = 'admin' | 'teacher' | 'student';
-
 @Entity({ name: 'role' })
 export class RoleEntity extends ParentEntity {
   @Column()
-  name: RoleType;
+  name: string;
 
   @OneToMany((type) => UserEntity, (user) => user.role)
   users: UserEntity[];
