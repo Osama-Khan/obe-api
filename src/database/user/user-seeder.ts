ead34d08768bb682e21bced1b66d582f8d03bf2b
Osama Khan <Osama6756@gmail.com>
2021-11-16 19:21:31 +0500

    Updated user seeder to use findOne for role search

--- src/database/user/user-seeder.ts
+++ src/database/user/user-seeder.ts
@@ -10,14 +10,14 @@ export default async function seedUsers(app: INestApplication) {
   const rsvc = app.get(RoleService);
   const roleUsers = [];
   for (const u of users) {
-    const roles = await rsvc.find({ where: { name: u.role } });
-    if (roles.length === 0) {
+    const role = await rsvc.findOne({ name: u.role });
+    if (!role) {
       LogHelper.error(`No such role "${u.role}"`);
       throw new Error(
         `The role "${u.role}" was not found. Please make sure the role name is correct in users seed file and that roles are seeded before users.`,
       );
     }
-    roleUsers.push({ ...u, role: { id: roles[0].id } });
+    roleUsers.push({ ...u, role: { id: role.id } });
   }
   await seed(svc, roleUsers, 'user');
 }
