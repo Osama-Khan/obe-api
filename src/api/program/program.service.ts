545e1b4de7e81905e7d6e12cbf75af1c441e965e
Osama Khan <Osama6756@gmail.com>
2021-12-03 22:01:46 +0500

    Removed extra import

--- src/api/program/program.service.ts
+++ src/api/program/program.service.ts
@@ -1,5 +1,4 @@
-import { PLOService } from '@api/objective/plo/plo.service';
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApiService } from '@shared/services/api.service';
 import { Repository } from 'typeorm';
@@ -9,7 +8,6 @@ import { ProgramEntity } from './program.entity';
 export class ProgramService extends ApiService<ProgramEntity> {
   constructor(
     @InjectRepository(ProgramEntity) repository: Repository<ProgramEntity>,
-    @Inject(PLOService) private ploService: PLOService,
   ) {
     super(repository);
   }
