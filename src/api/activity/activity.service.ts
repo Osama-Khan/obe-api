74b849418b7b2c3f7c9298d8e8be95db0d37ae7c
Osama Khan <Osama6756@gmail.com>
2021-12-04 06:51:28 +0500

    Added map inclusion in find method of activity

--- src/api/activity/activity.service.ts
+++ src/api/activity/activity.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApiService } from '@shared/services/api.service';
-import { Repository } from 'typeorm';
+import { FindManyOptions, In, Repository } from 'typeorm';
 import { ActivityEntity } from './activity.entity';
+import { ActivityMapEntity } from './map/map.entity';
 
 @Injectable()
 export class ActivityService extends ApiService<ActivityEntity> {
   constructor(
     @InjectRepository(ActivityEntity) repository: Repository<ActivityEntity>,
+    @InjectRepository(ActivityMapEntity)
+    private amRepository: Repository<ActivityMapEntity>,
   ) {
     super(repository);
+  }
+
+  /** Appends complete map objects if maps relation is included */
+  async find(
+    criteria?: FindManyOptions<ActivityEntity>,
+  ): Promise<ActivityEntity[]> {
+    const data = await this.repository.find(criteria);
+    const activities = [];
+    if (criteria?.relations?.includes('maps')) {
+      for (const c of data) {
+        const maps = await this.amRepository.find({
+          where: { id: In(c.maps.map((m) => m.id)) },
+          relations: ['clo'],
+        });
+        activities.push({ ...c, maps });
+      }
+    }
+    return activities.length > 0 ? activities : data;
   }
 }
