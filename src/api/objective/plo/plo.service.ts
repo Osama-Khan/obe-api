f37faaa47e05fd2c594431af65fd46d67c8869b3
Osama Khan <Osama6756@gmail.com>
2021-12-04 23:12:50 +0500

    Added default PLO order if criteria not defined

--- src/api/objective/plo/plo.service.ts
+++ src/api/objective/plo/plo.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ApiService } from '@shared/services/api.service';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { PLOEntity } from './plo.entity';
 
 @Injectable()
 export class PLOService extends ApiService<PLOEntity> {
   constructor(@InjectRepository(PLOEntity) repository: Repository<PLOEntity>) {
     super(repository);
+  }
+
+  async find(criteria?: FindManyOptions<PLOEntity>): Promise<PLOEntity[]> {
+    if (!criteria) criteria = { order: { number: 'ASC' } };
+    else if (!criteria.order) criteria.order = { number: 'ASC' };
+    return super.find(criteria);
   }
 }
