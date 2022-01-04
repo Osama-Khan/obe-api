08a08d83ce2625c13e9a91a35363f9bd5766134e
Osama Khan <Osama6756@gmail.com>
2022-01-04 07:24:50 +0500

    Improved user and user role seeding

--- src/database/user/user-seeder.ts
+++ src/database/user/user-seeder.ts
@@ -1,23 +1,9 @@
-import { RoleService } from '@api/role/role.service';
 import { UserService } from '@api/user/user.service';
 import { INestApplication } from '@nestjs/common';
-import { LogHelper } from '@shared/helpers';
 import seed from '../helpers';
 import * as users from './users.json';
 
 export default async function seedUsers(app: INestApplication) {
   const svc = app.get(UserService);
-  const rsvc = app.get(RoleService);
-  const roleUsers = [];
-  for (const u of users) {
-    const role = await rsvc.findOne({ name: u.role });
-    if (!role) {
-      LogHelper.error(`No such role "${u.role}"`);
-      throw new Error(
-        `The role "${u.role}" was not found. Please make sure the role name is correct in users seed file and that roles are seeded before users.`,
-      );
-    }
-    roleUsers.push({ ...u, role: { id: role.id } });
-  }
-  await seed(svc, roleUsers, 'user');
+  await seed(svc, users, 'user');
 }
