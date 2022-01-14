8961d7466246ebdb1832b4404543949284367b3c
osama-khan <osama6756@gmail.com>
2022-01-14 15:33:10 +0500

    Added student seeding

--- src/database/user/user-seeder.ts
+++ src/database/user/user-seeder.ts
@@ -1,9 +1,14 @@
 import { UserService } from '@api/user/user.service';
 import { INestApplication } from '@nestjs/common';
 import seed from '../helpers';
-import * as users from './users.json';
+import * as basicUsers from './users.json';
+import * as students from './students.json';
 
 export default async function seedUsers(app: INestApplication) {
+  const users = [
+    ...basicUsers,
+    students.map((s) => ({ ...s, role: 'student' })),
+  ];
   const svc = app.get(UserService);
   await seed(svc, users, 'user');
 }
