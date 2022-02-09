394c2edec2f5440fccd590ee70af8b7c3edb6151
Osama Khan <Osama6756@gmail.com>
2022-02-09 21:43:51 +0500

    Fixed CourseSeederModule entity imports

--- src/database/course/course-seeder.module.ts
+++ src/database/course/course-seeder.module.ts
@@ -1,10 +1,12 @@
 import { CourseEntity } from '@api/course/course.entity';
 import { CourseService } from '@api/course/course.service';
+import { CLOEntity } from '@api/objective/clo/clo.entity';
+import { ProgramEntity } from '@api/program/program.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CourseEntity])],
+  imports: [TypeOrmModule.forFeature([CourseEntity, CLOEntity, ProgramEntity])],
   providers: [CourseService],
 })
 export class CourseSeederModule {}
